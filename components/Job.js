6463ec952dfacaaf76fe8611086b1ceceaf0c181
Nick <njhuemmer@gmail.com>
2022-06-10 15:02:35 -0400

    update links

--- components/Job.js
+++ components/Job.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 const Job = ({ job }) => {
     return (
         <div className='mx-10 mb-4 mt-20 pl-16 pr-16'>
-            <p className='text-xl font-bold underline'>{job.title}</p>
+            <Link href={`/job/${job.id}`}>
+                <a className='text-xl font-bold underline'>{job.title}</a>
+            </Link>
             <h2 className='text-base font-normal mt-3'>{job.description}</h2>
             <div className='mt-4'>
                 <h4 className='inline'>Posted by</h4>
@@ -11,7 +13,7 @@ const Job = ({ job }) => {
                     <span>
                         {/* Link has the advantage of loading client-side code faster */}
                         <Link href={`/job/${job.id}`}>
-                            <a className='text-xl font-bold underline'>{job.title}</a>
+                            <a className='text-xl font-bold underline'>{job.author.name}</a>
                         </Link>
                     </span>
                 </div>
